3dec7995cf4e6639715b31c1c074f2550ac0a34d
WindrunnerMax <651525974@qq.com>
2024-02-11 20:37:35 +0800

    feat: post module

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,9 +1,24 @@
+import { Text } from "@tarojs/components";
 import { cs } from "laser-utils";
 import type { FC } from "react";
+
+import styles from "./index.modules.scss";
 
 export const Icon: FC<{
   type: string;
   size?: number;
+  className?: string;
+  space?: boolean;
 }> = props => {
-  return <i className={cs("shst-icon", `icon-${props.type}`)} style={{ fontSize: props.size }}></i>;
+  return (
+    <Text
+      className={cs(
+        "shst-icon",
+        `icon-${props.type}`,
+        props.space && styles.space,
+        props.className
+      )}
+      style={{ fontSize: props.size }}
+    ></Text>
+  );
 };
--- src/pages/index/index/index.tsx
+++ src/pages/index/index/index.tsx
@@ -1,9 +1,11 @@
-import { Button, Image, Swiper, SwiperItem, View } from "@tarojs/components";
+import { Button, Image, Navigator, RichText, Swiper, SwiperItem, View } from "@tarojs/components";
 import { cs, DateTime } from "laser-utils";
 import React, { useEffect, useState } from "react";
 
+import { Icon } from "@/components/icon";
 import { Layout } from "@/components/layout";
 import { Weather } from "@/components/weather";
+import { PATH } from "@/config/page";
 import { useMemoizedFn } from "@/hooks/use-memoized-fn";
 import { App } from "@/utils/app";
 import { Nav } from "@/utils/nav";
@@ -15,9 +17,13 @@ const NOW = new DateTime().format("yyyy-MM-dd K");
 
 export default function Index() {
   const [swiper, setSwiper] = useState<SwiperItemType[]>([]);
+  const [post, setPost] = useState("");
+  const [postUrl, setPostUrl] = useState("");
 
   const onInit = useMemoizedFn(() => {
     setSwiper(App.data.initData.ads);
+    setPost(App.data.initData.articalName);
+    setPostUrl(App.data.initData.articleUrl);
   });
 
   useEffect(() => {
@@ -27,7 +33,7 @@ export default function Index() {
   return (
     <React.Fragment>
       {/* `Banner` */}
-      <Layout topSpace>
+      <Layout>
         <View className={styles.swiperContainer}>
           <Swiper indicatorDots interval={5000} duration={1000} autoplay circular>
             {swiper.map((item, index) => (
@@ -44,6 +50,7 @@ export default function Index() {
       </Layout>
       {/* 天气 */}
       <Layout
+        topSpace
         title={NOW}
         captainSlot={
           <View className={styles.yCenter}>
@@ -57,7 +64,21 @@ export default function Index() {
         <Weather></Weather>
       </Layout>
       {/* 公告 */}
-      <Layout title="系统公告"></Layout>
+      <Layout title="系统公告">
+        <View className={cs(styles.article, "text-ellipsis")} onClick={() => Nav.webview(postUrl)}>
+          <Icon space type="gonggao"></Icon>
+          <RichText className="a-link" nodes={post}></RichText>
+        </View>
+        <Navigator
+          url={PATH.POST}
+          open-type="navigate"
+          className={cs(styles.article, "text-ellipsis")}
+          hover-class="none"
+        >
+          <Icon space type="gonggao"></Icon>
+          <text className="a-link">更多公告...</text>
+        </Navigator>
+      </Layout>
     </React.Fragment>
   );
 }
